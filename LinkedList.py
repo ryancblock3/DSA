839e16668a5f73390d956c31c1624fe00c113307
ryan <155644277+rbloc13@users.noreply.github.com>
2024-01-18 17:30:33 -0500

    Initial commit

---
+++ LinkedList.py
@@ -0,0 +1,179 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.length == 0:
+            return
+        temp = self.head
+        pre = self.head
+        while temp.next is not None:
+            pre = temp
+            temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length -1:
+            return self.pop
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+        
+        
+# Pre Append Testing #
+
+# my_linked_list = LinkedList(2)
+# my_linked_list.append(3)
+# my_linked_list.prepend(1)
+# my_linked_list.print_list()
+
+# Pop First Testing #
+
+# my_linkled_list = LinkedList(2)
+# my_linkled_list.append(1)
+# print(my_linkled_list.pop_first())
+# print(my_linkled_list.pop_first())
+# print(my_linkled_list.pop_first())
+
+# Get Testing #
+
+# my_linked_list = LinkedList(0)
+# my_linked_list.append(1)
+# my_linked_list.append(2)
+# my_linked_list.append(3)
+# print(my_linked_list.get(2))
+    
+# Set Testing #
+    
+# my_linked_list = LinkedList(11)
+# my_linked_list.append(3)
+# my_linked_list.append(23)
+# my_linked_list.append(7)
+# my_linked_list.set_value(1,4)
+# my_linked_list.print_list()
+    
+# Insert Testing
+    
+# my_linked_list = LinkedList(0)
+# my_linked_list.append(2)
+# my_linked_list.insert(1,1)
+# my_linked_list.print_list()
+    
+# Remove Testing
+    
+# my_linked_list = LinkedList(11)
+# my_linked_list.append(3)
+# my_linked_list.append(23)
+# my_linked_list.append(7)
+# print(my_linked_list.remove(2), '/n')
+# my_linked_list.print_list()
+            
+# Reverse Testing
+# my_linked_list = LinkedList(1)
+# my_linked_list.append(2)
+# my_linked_list.append(3)
+# my_linked_list.append(4)
+# my_linked_list.reverse()
+# my_linked_list.print_list()
