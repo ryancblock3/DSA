4353c311b17cf86ab1002419fa9eef387820f7c0
ryancblock3 <155644277+rbloc13@users.noreply.github.com>
2024-01-18 21:02:24 -0500

    added commenting

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,87 +1,107 @@
+# Node class represents a node in a doubly linked list
 class Node:
     def __init__(self, value):
         self.value = value
-        self.next = None
-        self.prev = None
+        self.next = None  # Reference to the next node in the list
+        self.prev = None  # Reference to the previous node in the list
 
+# DoublyLinkedList class represents a doubly linked list
 class DoublyLinkedList:
     def __init__(self, value):
+        # Initialize a new doubly linked list with a single node
         new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+        self.head = new_node  # Head points to the first node
+        self.tail = new_node  # Tail points to the last node
+        self.length = 1  # Length of the linked list
 
+    # Print the values of all nodes in the linked list
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
+    # Append a new node with the given value to the end of the linked list
     def append(self, value):
         new_node = Node(value)
         if self.length == 0:
+            # If the list is empty, set the new node as both head and tail
             self.head = new_node
             self.tail = new_node
         else:
+            # Otherwise, add the new node to the end and update references
             self.tail.next = new_node
             new_node.prev = self.tail
             self.tail = new_node
         self.length += 1
         return True
     
+    # Remove and return the last node from the linked list
     def pop(self):
         if self.length == 0:
             return None
         temp = self.tail
         if self.length == 1:
+            # If there's only one node, set head and tail to None
             self.head = None
             self.tail = None
         else:
+            # Otherwise, update tail and remove references
             self.tail = self.tail.prev
             self.tail.next = None
             temp.prev = None
         self.length -= 1
         return temp
     
+    # Add a new node with the given value to the beginning of the linked list
     def prepend(self, value):
         new_node = Node(value)
         if self.length == 0:
+            # If the list is empty, set the new node as both head and tail
             self.head = new_node
             self.tail = new_node
         else:
+            # Otherwise, add the new node to the beginning and update references
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
         self.length += 1
         return True
     
+    # Remove and return the first node from the linked list
     def pop_first(self):
         if self.length == 0:
             return None
         temp = self.head
         if self.length == 1:
+            # If there's only one node, set head and tail to None
             self.head = None
             self.tail = None
         else:
+            # Otherwise, update head and remove references
             self.head = self.head.next
             self.head.prev = None
             temp.next = None
         self.length -= 1
         return temp
     
+    # Get the node at the specified index in the linked list
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
         temp = self.head
         if index < self.length/2:
+            # Traverse from the beginning if the index is in the first half
             for _ in range(index):
                 temp = temp.next
         else:
+            # Traverse from the end if the index is in the second half
             temp = self.tail
             for _ in range(self.length - 1, index, -1):
                 temp = temp.prev
         return temp
     
+    # Set the value of the node at the specified index in the linked list
     def set_value(self, index, value):
         temp = self.get(index)
         if temp:
@@ -89,16 +109,20 @@ class DoublyLinkedList:
             return True
         return False
     
+    # Insert a new node with the given value at the specified index
     def insert(self, index, value):
         if index < 0 or index >= self.length:
             return False
         if index == 0:
+            # If inserting at the beginning, use the prepend method
             return self.prepend(value)
         if index == self.length:
-            return self.prepend(value)
+            # If inserting at the end, use the append method
+            return self.append(value)
         new_node = Node(value)
         before = self.get(index-1)
         after = before.next
+        # Update references to insert the new node at the specified index
         new_node.prev = before
         new_node.next = after
         before.next = new_node
@@ -106,34 +130,43 @@ class DoublyLinkedList:
         self.length += 1
         return True
     
+    # Remove and return the node at the specified index in the linked list
     def remove(self, index):
         if index < 0 or index >= self.length:
             return None
         if index == 0:
+            # If removing the first node, use the pop_first method
             return self.pop_first()
-        if index == self.length -1:
+        if index == self.length - 1:
+            # If removing the last node, use the pop method
             return self.pop()
         before = self.get(index - 1)
         after = self.get(index + 1)
+        # Update references to remove the node at the specified index
         before.next = after
         after.prev = before
         self.length -= 1
         return 
     
+    # Another method to remove and return the node at the specified index
     def remove_method_two(self, index):
         if index < 0 or index >= self.length:
             return None
         if index == 0:
+            # If removing the first node, use the pop_first method
             return self.pop_first()
-        if index == self.length -1:
+        if index == self.length - 1:
+            # If removing the last node, use the pop method
             return self.pop()
         temp = self.get(index)
+        # Update references to remove the node at the specified index
         temp.next.prev = temp.prev
         temp.prev.next = temp.next
         temp.next = None
         temp.prev = None
         self.length -= 1
         return temp
+
 
 
 # Testing Constructor
--- LinkedList.py
+++ LinkedList.py
@@ -1,37 +1,46 @@
+# Node class represents a node in a singly linked list
 class Node:
     def __init__(self, value):
         self.value = value
-        self.next = None
+        self.next = None  # Reference to the next node in the list
 
+# LinkedList class represents a singly linked list
 class LinkedList:
     def __init__(self, value):
+        # Initialize a new linked list with a single node
         new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+        self.head = new_node  # Head points to the first node
+        self.tail = new_node  # Tail points to the last node
+        self.length = 1  # Length of the linked list
 
+    # Print the values of all nodes in the linked list
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
+    # Append a new node with the given value to the end of the linked list
     def append(self, value):
         new_node = Node(value)
         if self.length == 0:
+            # If the list is empty, set the new node as both head and tail
             self.head = new_node
             self.tail = new_node
         else:
+            # Otherwise, add the new node to the end and update references
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
         return True
     
+    # Remove and return the last node from the linked list
     def pop(self):
         if self.length == 0:
-            return
+            return None
         temp = self.head
         pre = self.head
+        # Traverse to the last node while updating references
         while temp.next is not None:
             pre = temp
             temp = temp.next
@@ -43,17 +52,21 @@ class LinkedList:
             self.tail = None
         return temp
     
+    # Add a new node with the given value to the beginning of the linked list
     def prepend(self, value):
         new_node = Node(value)
         if self.length == 0:
+            # If the list is empty, set the new node as both head and tail
             self.head = new_node
             self.tail = new_node
         else:
+            # Otherwise, add the new node to the beginning and update references
             new_node.next = self.head
             self.head = new_node
         self.length += 1
         return True
     
+    # Remove and return the first node from the linked list
     def pop_first(self):
         if self.length == 0:
             return None
@@ -65,6 +78,7 @@ class LinkedList:
             self.tail = None
         return temp
     
+    # Get the node at the specified index in the linked list
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
@@ -73,6 +87,7 @@ class LinkedList:
             temp = temp.next
         return temp
     
+    # Set the value of the node at the specified index in the linked list
     def set_value(self, index, value):
         temp = self.get(index)
         if temp:
@@ -80,45 +95,56 @@ class LinkedList:
             return True
         return False
     
+    # Insert a new node with the given value at the specified index
     def insert(self, index, value):
         if index < 0 or index >= self.length:
             return False
         if index == 0:
+            # If inserting at the beginning, use the prepend method
             return self.prepend(value)
         if index == self.length:
+            # If inserting at the end, use the append method
             return self.append(value)
         new_node = Node(value)
         temp = self.get(index - 1)
+        # Update references to insert the new node at the specified index
         new_node.next = temp.next
         temp.next = new_node
         self.length += 1
         return True
     
+    # Remove and return the node at the specified index in the linked list
     def remove(self, index):
         if index < 0 or index >= self.length:
             return None
         if index == 0:
+            # If removing the first node, use the pop_first method
             return self.pop_first()
         if index == self.length -1:
-            return self.pop
+            # If removing the last node, use the pop method
+            return self.pop()
         prev = self.get(index - 1)
         temp = prev.next
+        # Update references to remove the node at the specified index
         prev.next = temp.next
         temp.next = None
         self.length -= 1
         return temp
     
+    # Reverse the linked list
     def reverse(self):
         temp = self.head
         self.head = self.tail
         self.tail = temp
         after = temp.next
         before = None
+        # Reverse the direction of links while traversing the list
         for _ in range(self.length):
             after = temp.next
             temp.next = before
             before = temp
             temp = after
+
 
         
         
