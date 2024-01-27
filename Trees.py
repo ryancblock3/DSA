5352a13eaeaeef9bcca71360fb67908164631ca0
ryan <155644277+rbloc13@users.noreply.github.com>
2024-01-26 19:38:54 -0500

    bubble sort, insertion sort, merge sort, selection sort

--- Trees.py
+++ Trees.py
@@ -111,6 +111,54 @@ class BinarySearchTree:
     def delete_node(self, value):
         self.root = self.__delete_node(self.root, value)
 
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop()
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+    
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+    
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):    
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+    
+    def dfs_in_order(self):
+        results = []
+        def traverse(current_node):    
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
 
 # Testing Constructor
         
@@ -182,14 +230,62 @@ class BinarySearchTree:
         
 # Testing Delete
         
-my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
-print('Root:', my_tree.root.value)
-print('Root --> Left:', my_tree.root.left.value)
-print('Root --> Right:', my_tree.root.right.value)
-my_tree.delete_node(2)
-print('Root:', my_tree.root.value)
-print('Root --> Left:', my_tree.root.left.value)
-print('Root --> Right:', my_tree.root.right.value) # Cant figure out why I am getting AttributeError
+# my_tree = BinarySearchTree()
+# my_tree.insert(2)
+# my_tree.insert(1)
+# my_tree.insert(3)
+# print('Root:', my_tree.root.value)
+# print('Root --> Left:', my_tree.root.left.value)
+# print('Root --> Right:', my_tree.root.right.value)
+# my_tree.delete_node(2)
+# print('Root:', my_tree.root.value)
+# print('Root --> Left:', my_tree.root.left.value)
+# print('Root --> Right:', my_tree.root.right.value) # Cant figure out why I am getting AttributeError
+    
+# Testing Breadth First Search
+    
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
+# print(my_tree.BFS())
+    
+# Testing Depth First Search Pre Order
+    
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
+# print(my_tree.dfs_pre_order())
+    
+# Testing Depth First Search Post Order
+    
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
+# print(my_tree.dfs_post_order())
+    
+# Testing Depth First Search In Order
+    
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
+# print(my_tree.dfs_in_order())
